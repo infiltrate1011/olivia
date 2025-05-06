a69d59a62529caae825e2d103a004eedf962f996
Brandon Forant <brandon@INFI1TRATE.local>
2025-05-06 19:40:24 -0400

    Fix: allow iframe embedding by setting X-Frame-Options

--- pages/api/embed.js
+++ pages/api/embed.js
@@ -1,23 +1,26 @@
 export default function handler(req, res) {
   res.setHeader("Content-Type", "text/html");
   res.setHeader("X-Frame-Options", "ALLOWALL");
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
-  res.end(`
+  res.status(200).send(`
     <!DOCTYPE html>
-    <html>
+    <html lang="en">
       <head>
+        <meta charset="UTF-8" />
+        <title>Olivia Chat</title>
         <style>
           html, body {
             margin: 0;
             padding: 0;
-            width: 100%;
             height: 100%;
+            overflow: hidden;
           }
         </style>
       </head>
       <body>
-        <script type="module" src="/widget.js"></script>
+        <script src="/widget.js"></script>
       </body>
     </html>
-  `);
+  `));
 }
