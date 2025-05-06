7673729611c8851b2b5e719a77d63bde8492b647
Brandon Forant <brandon@INFI1TRATE.local>
2025-05-06 16:25:38 -0400

    Fix CSP to include full Shopify subdomain support

--- next.config.js
+++ next.config.js
@@ -7,12 +7,13 @@ const nextConfig = {
         source: "/(.*)",
         headers: [
           {
-            key: "Content-Security-Policy",
-            value: "frame-ancestors 'self' https://*.myshopify.com https://myshopify.com;",
-          },
-          {
             key: "X-Frame-Options",
             value: "ALLOWALL",
+          },
+          {
+            key: "Content-Security-Policy",
+            value:
+              "frame-ancestors 'self' https://*.myshopify.com https://myshopify.com https://fruitstand-skin.myshopify.com https://www.fruitstand-skin.myshopify.com",
           },
         ],
       },
