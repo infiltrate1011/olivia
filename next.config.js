dbc150eeecd3adc591569cfa15c5ec6be99ed90c
Brandon Forant <brandon@INFI1TRATE.local>
2025-05-06 16:13:00 -0400

    Update CSP to allow Shopify iframe embed

--- next.config.js
+++ next.config.js
@@ -1,17 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   async headers() {
     return [
       {
         source: "/(.*)",
         headers: [
           {
-            key: "X-Frame-Options",
-            value: "ALLOWALL",
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' https://*.myshopify.com https://myshopify.com;",
           },
           {
-            key: "Content-Security-Policy",
-            value: "frame-ancestors https://*.myshopify.com https://myshopify.com;",
+            key: "X-Frame-Options",
+            value: "ALLOWALL",
           },
         ],
       },
